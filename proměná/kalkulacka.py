0c0e5d7fb3caabb19501f2f6df3ab5249a9ac4c6
Tobiáš Šimon - IT2A <tobiassi255@student.voskh.cz>
2026-09-24 09:27:29 +0200

    Add basic calculator and main program for user input

---
+++ proměná/kalkulacka.py
@@ -0,0 +1,4 @@
+cislo1=input("Zadej 1. číslo: ")
+cislo2=input("Zadej 2. číslo: ")
+vysledek= float(cislo1) + float(cislo2)
+print("Výsledek: " + str(vysledek))
---
+++ proměná/main.py
@@ -0,0 +1,8 @@
+jmeno=input("Zadej své jméno: ")
+podtrzeni="-" * 20
+print("")
+print(podtrzeni)
+print("Jméno:" + jmeno)
+print("Obor: IT")
+print(podtrzeni)
+print("")
